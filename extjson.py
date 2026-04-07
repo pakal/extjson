e143845d7c2c57a46ed54d8aefb3c9ad7a495a9b
Pascal Chambon <pythoniks@gmail.com>
2026-04-07 17:37:32 +0200

    refactor: enhance extjson.py with Micorpython support

--- extjson.py
+++ extjson.py
@@ -23,12 +23,12 @@ SOFTWARE.
 
 from __future__ import annotations
 
-import base64
-import calendar
+import binascii
 import datetime
-import decimal
 import json
 import math
+import sys
+
 import uuid
 from typing import (
     Any,
@@ -38,9 +38,19 @@ from typing import (
     Union,
 )
 
+try:
+    from decimal import Decimal
+except ImportError:
+    # E.G on Micropython
+    class _FakeDecimal: pass
+    Decimal = _FakeDecimal
 
-# Boundary between int32 and int64 for canonical representation of integers
+
 _INT32_MAX = 2**31
+
+UTF8_ENCODING = "utf8"
+
+IS_MICROPYTHON = (sys.implementation.name == "micropython")
 
 
 # Only these two binary-data subtypes are supported
@@ -48,21 +58,24 @@ BINARY_SUBTYPE = 0
 UUID_SUBTYPE = 4
 
 
-# Default encoding for JSON strings when working with bytes
-UTF8_ENCODING = "utf8"
+
+# NOTE: we can't provide a "default" handler to use with json.dumps(),
+# for non-canonical mode, because of the way NaN/-Inf/+Inf are handled
+# without resorting to default()...
 
 
-# LOW LEVEL UTILITIES #
+def extjson_decoder_object_hook(obj: dict) -> Any:
+    return _convert_primitive_from_extjson_dict(obj)
 
 
-def convert_to_extjson(obj: Any, canonical: bool = False) -> Any:
+def convert_to_extjson(obj: Any, canonical: bool=True) -> Any:
     """Recursive helper method that converts BSON types so they can be
     converted into json.
     """
     assert canonical in (True, False), canonical
     if isinstance(obj, dict):
         return {k: convert_to_extjson(v, canonical=canonical) for k, v in obj.items()}
-    elif isinstance(obj, list):  # Tuples are not handled!
+    elif isinstance(obj, (list, tuple)):  # Tuples are treated like lists!
         return [convert_to_extjson(v, canonical=canonical) for v in obj]
 
     return _convert_primitive_to_extjson(obj, canonical=canonical)
@@ -107,18 +120,19 @@ def _convert_primitive_from_extjson_dict(ext_obj_dict: Mapping[str, Any]) -> Any
     assert isinstance(ext_obj_dict, dict), repr(ext_obj_dict)
     match = None
     if len(ext_obj_dict) != 1:
-        return ext_obj_dict  # Not a {$type: ...} dict
+        return ext_obj_dict  # We assume it's not a {$type: ...} dict
     for k in ext_obj_dict:
         if k in _PARSERS:
             match = k
             break
     if match:
         return _PARSERS[match](ext_obj_dict)
-    return ext_obj_dict
+    return ext_obj_dict  # Leave it untouched
 
 
 def _encode_canonical_binary(data: bytes, subtype: int) -> Any:
-    return {"$binary": {"base64": base64.b64encode(data).decode(), "subType": "%02x" % subtype}}
+    return {"$binary": {"base64": _simple_b64encode(data).decode('ascii'),
+                        "subType": "%02x" % subtype}}
 
 
 def _encode_int(obj: int, canonical: bool) -> Any:
@@ -146,7 +160,7 @@ def _encode_float(obj: float, canonical: bool) -> Any:
     return obj
 
 
-def _encode_decimal(obj: decimal.Decimal, canonical: bool) -> dict:
+def _encode_decimal(obj: Decimal, canonical: bool) -> dict:
     # Always use canonical representation for Decimal numbers
     return {"$numberDecimal": str(obj)}
 
@@ -155,13 +169,15 @@ def _encode_datetime(obj: datetime.datetime, canonical: bool) -> dict:
     if not _is_aware_datetime(obj):
         raise TypeError(f"Unsupported naive datetime encountered: {obj}")
     if canonical:
-        millis = _datetime_to_millis(obj)
+        millis = _aware_datetime_to_millis(obj)
         return {"$date": {"$numberLong": str(millis)}}
     # We output datetime as "YYYY-MM-DDTHH:MM:SS[.fff]<offset>" (not microseconds)
     timespec = "milliseconds" if obj.microsecond != 0 else "seconds"
     dts = obj.isoformat(sep="T", timespec=timespec)
     dts = dts.replace("+00:00", "Z")  # We ASSUME that 0-offset means UTC...
-    return {"$date": dts}
+    return {
+        "$date": dts
+    }
 
 
 def _encode_bytes(obj: bytes, canonical: bool) -> dict:
@@ -185,27 +201,28 @@ _ENCODERS: dict[Type, Callable[[Any, bool], Any]] = {
     uuid.UUID: _encode_uuid,
     datetime.datetime: _encode_datetime,
     float: _encode_float,
-    decimal.Decimal: _encode_decimal,
+    Decimal: _encode_decimal,
     int: _encode_int,
     str: _encode_noop,
     type(None): _encode_noop,
 }
+
 
 _EXTENDED_JSON_BUILT_IN_TYPES = tuple(_ENCODERS)
 
 
 def _parse_canonical_binary(doc: Any) -> Union[bytes, uuid.UUID]:
     binary = doc["$binary"]
+    if not isinstance(binary, dict) or set(binary.keys()) != {"base64", "subType"}:
+        raise TypeError(f"$binary must be a dict with keys 'base64' and 'subType' only: {doc}")
     b64 = binary["base64"]
     subtype = binary["subType"]
     if not isinstance(b64, str):
         raise TypeError(f"$binary base64 must be a string: {doc}")
     if not isinstance(subtype, str) or len(subtype) > 2:
-        raise TypeError(f"$binary subType must be a string at most 2 characters: {doc}")
-    if len(binary) != 2:
-        raise TypeError(f'$binary must include only "base64" and "subType" components: {doc}')
+        raise TypeError(f"$binary subType must be a string with at most 2 characters: {doc}")
 
-    data = base64.b64decode(b64.encode())
+    data = _simple_b64decode(b64.encode('ascii'))
     return _get_as_binary_or_uuid(data, int(subtype, 16))
 
 
@@ -217,43 +234,60 @@ def _get_as_binary_or_uuid(data: Any, subtype: int) -> Union[bytes, uuid.UUID]:
     return data
 
 
-def _parse_canonical_datetime(doc: Any) -> datetime.datetime:
-    """Decode a JSON datetime to python datetime.datetime."""
+def _parse_canonical_datetime(
+    doc: Any
+) -> datetime.datetime:
+    """Decode a JSON datetime to python datetime.datetime.
+
+    Canonical dates are always returned as UTC, whereas isoformat strings are
+    returned with their indicated timezone (which MUST exist)."""
     dtm = doc["$date"]
-    if len(doc) != 1:
-        raise TypeError(f"Bad $date, extra field(s): {doc}")
+    if not isinstance(dtm, (str, int)):
+        raise TypeError(f"Date must be iso string or millisecond int: {doc}")
     if isinstance(dtm, str):
-        return datetime.datetime.fromisoformat(dtm)
-    return _millis_to_datetime(dtm)
-
-
-def _get_single_str_field(doc: Any, key: str) -> str:
-    value = doc[key]
-    if len(doc) != 1:
-        raise TypeError(f"Bad {key}, extra field(s): {doc}")
-    if not isinstance(value, str):
-        raise TypeError(f"{key} must be a string: {doc}")
-    return value
+        res = datetime.datetime.fromisoformat(dtm)
+        if not _is_aware_datetime(res):
+            raise TypeError(f"Unsupported naive datetime: {dtm}")
+        return res
+    return _millis_to_utc_datetime(dtm)
 
 
 def _parse_canonical_int32(doc: Any) -> int:
-    return int(_get_single_str_field(doc, "$numberInt"))
+    """Decode a JSON int32 to python int."""
+    i_str = doc["$numberInt"]
+    if not isinstance(i_str, str):
+        raise TypeError(f"$numberInt must be string: {doc}")
+    return int(i_str)
 
 
 def _parse_canonical_int64(doc: Any) -> int:
-    return int(_get_single_str_field(doc, "$numberLong"))
+    """Decode a JSON int64 to bson.int64.Int64."""
+    l_str = doc["$numberLong"]
+    if not isinstance(l_str, str):
+        raise TypeError(f"$numberLong must be string: {doc}")
+    return int(l_str)  # No need for Int64 type here
 
 
 def _parse_canonical_double(doc: Any) -> float:
-    return float(_get_single_str_field(doc, "$numberDouble"))
+    """Decode a JSON double to python float."""
+    d_str = doc["$numberDouble"]
+    if not isinstance(d_str, str):
+        raise TypeError(f"$numberDouble must be string: {doc}")
+    return float(d_str)
 
 
-def _parse_canonical_decimal(doc: Any) -> decimal.Decimal:
-    return decimal.Decimal(_get_single_str_field(doc, "$numberDecimal"))
+def _parse_canonical_decimal(doc: Any) -> Decimal:
+    d_str = doc["$numberDecimal"]
+    if not isinstance(d_str, str):
+        raise TypeError(f"$numberDecimal must be string: {doc}")
+    return Decimal(d_str)
 
 
 def _parse_legacy_uuid(doc: Any) -> Union[bytes, uuid.UUID]:
-    return uuid.UUID(_get_single_str_field(doc, "$uuid"))
+    """Decode a JSON legacy $uuid to Python UUID."""
+    if not isinstance(doc["$uuid"], str):
+        raise TypeError(f"$uuid must be a string: {doc}")
+    return uuid.UUID(doc["$uuid"])
 
 
 _PARSERS: dict[str, Callable[[Any], Any]] = {
@@ -276,36 +310,55 @@ def _is_aware_datetime(dt: datetime.datetime) -> bool:
     return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
 
 
-def _datetime_to_millis(dt: datetime.datetime) -> int:
-    """Convert aware datetime to milliseconds since epoch UTC."""
+def _assert_is_aware_datetime(dt: datetime.datetime):
     assert _is_aware_datetime(dt), f"Unsupported naive datetime encountered: {dt}"
-    dt = dt - dt.utcoffset()  # type: ignore
-    return int(calendar.timegm(dt.timetuple()) * 1000 + dt.microsecond // 1000)
 
 
-def _millis_to_datetime(
+def _aware_datetime_to_millis(dt: datetime.datetime) -> int:
+    """Convert aware datetime to milliseconds since epoch UTC."""
+    _assert_is_aware_datetime(dt)
+    timestamp_ms = dt.timestamp() * 1000
+    return math.floor(timestamp_ms)
+
+
+def _millis_to_utc_datetime(
     millis: int,
 ) -> datetime.datetime:
     """Convert milliseconds since epoch UTC to aware datetime."""
     assert isinstance(millis, int), repr(millis)
-    diff = ((millis % 1000) + 1000) % 1000
-    seconds = (millis - diff) // 1000
-    micros = diff * 1000
-
+    seconds = millis // 1000
+    micros = (millis % 1000) * 1000
     dt = _EPOCH_AWARE + datetime.timedelta(seconds=seconds, microseconds=micros)
-
     return dt  # UTC aware datetime
+
+
+def _simple_b64encode(s):
+    """Encode the bytes-like object s using Base64 and return a bytes object.
+    """
+    encoded = binascii.b2a_base64(s, newline=False)
+    return encoded
+
+
+def _simple_b64decode(s):
+    """Decode the Base64 encoded bytes-like object or ASCII string s.
+
+    The result is returned as a bytes object.  A binascii.Error is raised if
+    s is incorrectly padded.
+    """
+    assert isinstance(s, bytes)
+    return binascii.a2b_base64(s)
 
 
 # HIGH LEVEL UTILITIES #
 
 
-def dumps(obj: Any, *args: Any, canonical=False, **kwargs: Any) -> str:
+def dumps(obj: Any, *args: Any, canonical=True, **kwargs: Any) -> str:
     """Helper function that wraps :func:`json.dumps`.
 
     Recursive function that handles main ExtendedJSON types.
     """
     ext_obj = convert_to_extjson(obj, canonical=canonical)
+    print("CALLING JSON DUMPS", args, kwargs)
     return json.dumps(ext_obj, *args, **kwargs)
 
 
@@ -320,12 +373,12 @@ def loads(s: Union[str, bytes, bytearray], *args: Any, **kwargs: Any) -> Any:
 
 def dump_to_json_str(data, **extra_options):
     """
-    Dump a data tree to a json representation as string
-    (with sort_keys=True by default).
+    Dump a data tree to a json representation as string.
     Supports advanced types like bytes, uuids, dates...
     """
-    sort_keys = extra_options.pop("sort_keys", True)
-    json_str = dumps(data, sort_keys=sort_keys, **extra_options)
+    if not IS_MICROPYTHON:  # Unsupported by mypy for now
+        extra_options.setdefault("sort_keys", True)
+    json_str = dumps(data, **extra_options)
     return json_str
 
 
@@ -334,7 +387,7 @@ def load_from_json_str(data, **extra_options):
     Load a data tree from a json representation as string.
     Supports advanced types like bytes, uuids, dates...
 
-    Raises exceptions.ValidationError on loading error.
+    Raises JSONDecodeError or TypeError on loading/coercion error.
     """
     assert isinstance(data, str), data
     return loads(data, **extra_options)
